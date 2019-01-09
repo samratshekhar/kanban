539841821a3f59ba9deca30d96335a3c40c0d7b1
Samrat <samratshekhar42@gmail.com>
2019-01-09 23:52:33 +0530

    implement add note functionality

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import uuid from 'uuid';
 
 import Notes from './Notes';
 import { data } from '../data';
 
-export default () => (
-  <div>
-    <button onClick={() => alert('new note added')}>Add new note</button>
-    <Notes notes={ data } />
-  </div>
-);
+export default class App extends React.Component{
+  state = { notes: data };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.addNote}>Add new note</button>
+        <Notes notes={ this.state.notes } />
+      </div>
+    );
+  }
+
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat({
+        id: uuid.v4(),
+        task: 'New task',
+      }),
+    });
+  }
+}
